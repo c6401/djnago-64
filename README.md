9d509fe45582ce3e8d71d19a3798b3c76df1db38
Ruslan Zhenetl <>
2018-11-20 23:24:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# django-64
+
+## Installation
+```
+sudo python -m pip install git+https://github.com/c6401/django-64
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='djnago-64',
+    version='0.0.1',
+    description='python-blocks, small reusable code blocks',
+    long_description=open('README.md').read(),
+    author='Ruslan Zhenetl',
+    url='https://github.com/c6401/django-64',
+    license='MIT',
+    packages=['django_64'],
+)
